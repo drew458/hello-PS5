391e04f7ea28547ce5d2be7252b5b368769ac9ca
drew <andry.marini@gmail.com>
2021-07-10 23:04:50 +0200

    fixed boolean condition

--- checkStrings.py
+++ checkStrings.py
@@ -2,9 +2,11 @@ def checkH1(soup_h1, texth1):
     for i in soup_h1:
         if i.string == texth1:
             return True
+    return False
 
 
 def checkH3(soup_h3, texth3):
     for i in soup_h3:
         if i.string == texth3:
             return True
+    return False
