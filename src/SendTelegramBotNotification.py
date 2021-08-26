10e0cca0ff30c646aed7743fe4393e83c6ddf572
drew <andry.marini@gmail.com>
2021-08-26 11:49:38 +0200

    added input arguments and fixed bugs

--- src/SendTelegramBotNotification.py
+++ src/SendTelegramBotNotification.py
@@ -4,15 +4,22 @@ import urllib
 import requests
 
 
-def sendNotification(found_message):
+def sendNotification(token, chat_id, found_message):
     """
     Sends a Telegram notification via a bot.
     """
 
     # Retain the Telegram Bot token from environment variables
-    TOKEN = os.environ["TOKEN"]
+    try:
+        TOKEN = os.environ["TOKEN"]
+    except KeyError:
+        TOKEN = token
+
     # Retain the Telegram chat_id from environment variables
-    CHAT_ID = os.environ["CHAT_ID"]
+    try:
+        CHAT_ID = os.environ["CHAT_ID"]
+    except KeyError:
+        CHAT_ID = chat_id
 
     # Send notification
     notification_url = 'https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (
